d488ec5f2742215430987385edeb08f4764b3581
Kael Spencer <kaels@microsoft.com>
2018-09-12 11:31:38 -0700

    Support package, user, and project settings. Update menu settings edit command.

--- OpenOnWeb.py
+++ OpenOnWeb.py
@@ -4,11 +4,18 @@ import webbrowser
 import sublime
 import sublime_plugin
 
+PLUGIN_NAME = 'OpenOnWeb'
+GIT_COMMAND_KEY = 'git_command'
+BASE_URL_KEY = 'base_url'
+
 
 class OpenOnWeb(sublime_plugin.WindowCommand):
-    def __init__(self, arg):
-        sublime_plugin.WindowCommand.__init__(self, arg)
-        self.config = []
+    def __init__(self, window):
+        sublime_plugin.WindowCommand.__init__(self, window)
+
+        print("---------------------Loading OpenOnWeb---------------------")
+        print("git_command: {}".format(self.get_setting(GIT_COMMAND_KEY)))
+        print("base_url: {}".format(self.get_setting(BASE_URL_KEY)))
 
     def run(self):
         filename = self.window.active_view().file_name()
@@ -17,22 +24,24 @@ class OpenOnWeb(sublime_plugin.WindowCommand):
         filename = filename.replace("\\", "/")
 
         if len(filename) > 0:
-            self.get_config()
             git_root = self.get_git_root(filename)
-            relative = self.get_git_relative_path(git_root, filename)
+            relative = get_git_relative_path(git_root, filename)
 
-            url = self.config["base_url"] + relative
+            url = self.get_setting(BASE_URL_KEY) + relative
             print(url)
-            self.open_in_browser(url)
+            open_in_browser(url)
 
-    def get_config(self):
-        # TODO: Coalesce with global settings.
-        self.config = self.window.project_data()["OpenOnWeb"]
-        print(self.config)
+    def get_setting(self, key):
+        settings = sublime.load_settings("OpenOnWeb.sublime-settings")
+        project_data = self.window.project_data()
+
+        if project_data is not None and project_data[PLUGIN_NAME] is not None and key in project_data[PLUGIN_NAME]:
+            return project_data[PLUGIN_NAME][key]
+        return settings.get(key)
 
     def get_git_root(self, filename):
         cwd = os.path.dirname(filename)
-        psd = subprocess.Popen([self.config["git_command"], 'rev-parse', "--show-toplevel"], shell=True, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        psd = subprocess.Popen([self.get_setting(GIT_COMMAND_KEY), 'rev-parse', "--show-toplevel"], shell=True, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         root = str(psd.communicate()[0])
 
         # Path may have trailing escaped \n', remove.
@@ -48,13 +57,14 @@ class OpenOnWeb(sublime_plugin.WindowCommand):
             raise Exception("Problem determining git repo root.")
         return root
 
-    def get_git_relative_path(self, git_root, filename):
-        # Ensure filename is under git_root. Pathlib would be the proper thing to do here, but it isn't available in Sublime Text.
-        if not filename.startswith(git_root):
-            print("Git root: {}\nSelected file: {}".format(git_root, filename))
-            raise Exception("Selected file is not under git_root.")
-        return filename[len(git_root):]
 
-    def open_in_browser(self, url):
-        # command = "{} {}".format("chrome.exe", url)
-        webbrowser.open_new_tab(url)
+def get_git_relative_path(git_root, filename):
+    # Ensure filename is under git_root. Pathlib would be the proper thing to do here, but it isn't available in Sublime Text.
+    if not filename.startswith(git_root):
+        print("Git root: {}\nSelected file: {}".format(git_root, filename))
+        raise Exception("Selected file is not under git_root.")
+    return filename[len(git_root):]
+
+
+def open_in_browser(url):
+    webbrowser.open_new_tab(url)
